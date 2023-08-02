6fbd6c8070065ac00a8698a899a696c908c829e0
aykutparlak1 <aykutparlak20@gmail.com>
2023-08-02 21:15:46 +0300

    refactor EfRentalDal

--- DataAccess/Concrete/EntityFramework/EfRentalDal.cs
+++ DataAccess/Concrete/EntityFramework/EfRentalDal.cs
@@ -2,12 +2,13 @@
 using DataAccess.Abstract;
 using Entities.Concrete;
 using Entities.DTOs;
+using System.Linq;
 
 namespace DataAccess.Concrete.EntityFramework
 {
     public class EfRentalDal : EfEntityRepositoryBase<Rental, SqlContext>, IRentalDal
     {
-        public List<RentalsDetailDto> GetRentalsDetailById(int Id)
+        public RentalsDetailDto GetRentalsDetailById(int Id)
         {
             using (SqlContext context = new SqlContext())
             {
@@ -15,9 +16,10 @@ namespace DataAccess.Concrete.EntityFramework
                              where rental.Id == Id
                              join car in context.Cars on rental.CarId equals car.Id
                              join customer in context.Customers on rental.CustomerId equals customer.Id
-                             select new RentalsDetailDto { RentalId = rental.Id, CarName = car.CarName, CarDescription = car.Description, ModelYear = car.ModelYear, DailyPrice = car.DailyPrice, CustomerName = customer.CompanyName, IsActive=car.IsActive };
-
-                return result.ToList(); ;
+                             select new RentalsDetailDto { RentalId = rental.Id, CarName = car.CarName,
+                              CarDescription = car.Description, ModelYear = car.ModelYear, DailyPrice = car.DailyPrice,
+                              CustomerName = customer.CompanyName, IsActive=car.IsActive };
+                return result.SingleOrDefault();
             }
         }
     }
